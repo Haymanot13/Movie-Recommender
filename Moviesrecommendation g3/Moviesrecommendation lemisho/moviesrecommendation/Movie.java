77d6e8e1ba123dd9f5b2e453076d9aa31d34b510
BilalX570 <bilalmuktar192@gmail.com>
2025-12-31 22:08:12 -1000

    update Movie  with additional properties and methods

--- Moviesrecommendation g3/Moviesrecommendation lemisho/moviesrecommendation/Movie.java
+++ Moviesrecommendation g3/Moviesrecommendation lemisho/moviesrecommendation/Movie.java
@@ -5,48 +5,142 @@ import java.io.Serializable;
 public class Movie implements Serializable {
     private int id;
     private String title;
-    private String name; // For TV Series
     private String overview;
-    private String poster_path;
-    private double vote_average;
-    private String release_date;
-    private String first_air_date; // For TV Series
-    private String original_language;
-    private String media_type; // "movie" or "tv"
+    private String releaseDate;
+    private String posterPath;
+    private String backdropPath;
+    private double voteAverage;
+    private int voteCount;
+    private double popularity;
+    private String mediaType;
+    private String[] genreIds;
+    private int runtime;
+    private String originalLanguage;
+    private String originalTitle;
+
+    public Movie() {}
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getTitle() {
-        return title != null ? title : name;
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getOverview() {
         return overview;
     }
 
-    public String getPosterPath() {
-        return poster_path;
-    }
-
-    public double getVoteAverage() {
-        return vote_average;
+    public void setOverview(String overview) {
+        this.overview = overview;
     }
 
     public String getReleaseDate() {
-        return release_date != null ? release_date : first_air_date;
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public String[] getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(String[] genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
     }
 
     public String getOriginalLanguage() {
-        return original_language;
+        return originalLanguage;
     }
-    
-    public String getMediaType() {
-        return media_type;
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
     }
-    
-    public void setMediaType(String mediaType) {
-        this.media_type = mediaType;
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", overview='" + overview + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", voteAverage=" + voteAverage +
+                ", mediaType='" + mediaType + '\'' +
+                '}';
     }
 }
